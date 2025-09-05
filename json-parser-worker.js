6d07aeb58ab788e276b6fbbd1f28b49af84c7992
Lewis Keen <yeggstry@gmail.com>
2025-09-05 11:54:05 +0100

    fix: refactored to re-enable custom file handling, minor QOL improvements

--- json-parser-worker.js
+++ json-parser-worker.js
@@ -1,9 +1,18 @@
+import { processShardSeriesData } from './shard-data-processor.js';
+
 self.onmessage = function (event) {
-    const rawJsonString = event.data;
+    const { rawData, seriesSafeName, processType } = event.data;
+
     try {
-        const parsedData = JSON.parse(rawJsonString);
-        self.postMessage({ status: 'complete', data: parsedData });
+        const parsedData = JSON.parse(rawData);
+
+        if (processType === "custom_file") {
+            const processedData = processShardSeriesData(seriesSafeName, parsedData);
+            self.postMessage({ status: "complete", data: processedData, seriesSafeName });
+        } else {
+            self.postMessage({ status: "complete", data: parsedData });
+        }
     } catch (error) {
-        self.postMessage({ status: 'error', message: error.message });
+        self.postMessage({ status: "error", message: error.message });
     }
 };
