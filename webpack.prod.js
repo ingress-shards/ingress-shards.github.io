d4e004c2c3d154ecb33bd8ebe89b15b0167981bf
Lewis Keen <yeggstry@gmail.com>
2026-01-31 03:56:45 +0000

    feat(build): decouple version string generation in dev and production configs

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,10 +1,10 @@
 import { merge } from 'webpack-merge';
-import common from './webpack.common.js';
+import common, { packageJson } from './webpack.common.js';
 
 const REPO_NAME = '/shards/';
 
 export default (env) => {
-    return merge(common(env), {
+    return merge(common(env, { appVersion: packageJson.version }), {
         mode: 'production',
         output: {
             publicPath: REPO_NAME,
